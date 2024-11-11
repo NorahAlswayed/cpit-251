01bcf9eccad913181cff4ab2e5ae241146274a90
NorahAlswayed <norahalswayed@gmail.com>
2024-11-11 17:09:41 +0300

    Update Cpit_251.java
    I added my name

--- Cpit_251.java
+++ Cpit_251.java
@@ -17,6 +17,7 @@ public class Cpit_251 {
     public static void main(String[] args) {
   System.out.print("hello");
   System.out.print("refal");
+     System.out.print("norah");     
     }
     
 }
