16bfda9ce3401d2c309fae99d571fcfa727153c1
AljoharahAloraini <aaloraini0010@stu.kau.edu.sa>
2024-11-11 17:01:34 +0300

    Update Cpit_251.java
    غيرت الجملة

--- Cpit_251.java
+++ Cpit_251.java
@@ -15,7 +15,7 @@ public class Cpit_251 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-  System.out.print("hi");
+  System.out.print("hello");
     }
     
 }
